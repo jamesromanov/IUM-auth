37d3a0d0378ddac3fedef6f7b9ecede97fb84373
Jamshid Qahramonov <qahramonovjamshid1@gmail.com>
2025-07-10 02:51:39 +0500

    fixed login user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,7 +5,11 @@ const errorHandler = require("../utils/error.handler");
 const registerUser = async (data) => {
   const userExists = await User.findOne({ where: { email: data.email } });
   if (userExists) throw new Error("User already registered");
-  return await User.create(data);
+  const userCraeted = (await User.create(data)).toJSON();
+  delete userCraeted.refreshToken;
+  delete userCraeted.password;
+  delete userCraeted.role;
+  return userCraeted;
 };
 
 const loginUser = async (data) => {
@@ -16,7 +20,10 @@ const loginUser = async (data) => {
     userExists.password
   );
   if (!comparePassword) throw new Error("Invalid email or password");
-  console.log(userExists);
+  const payload = {
+    id: userExists.id,
+    role: userExists.role,
+  };
 };
 
 module.exports = { registerUser, loginUser };
